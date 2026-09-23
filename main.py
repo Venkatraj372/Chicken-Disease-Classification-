dd5f8c3159b517d59c8756339d8315396f1fbc96
Venkatraj372 <venkatraj23122000@gmail.com>
2026-09-23 18:54:17 +0530

    Model Training Complete

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from CNNClassifier.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from CNNClassifier import logger
 from CNNClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from CNNClassifier.pipeline.stage_03_train_model import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
 
 try:
     logger.info(f">>>>> stage{STAGE_NAME}  started <<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\n X====================X")
 except Exception as e:
     logger.exception(e)
@@ -19,10 +20,22 @@ STAGE_NAME = "Prepare Base Model"
 
 try:
     logger.info(f">>>>> stage{STAGE_NAME}  started <<<<<")
-    obj = PrepareBaseModelTrainingPipeline()
-    obj.main()
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\n X====================X")
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Train Model"
+
+try:
+    logger.info(f">>>>> stage{STAGE_NAME}  started <<<<<")
+    model_train = ModelTrainingPipeline()
+    model_train.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\n X====================X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
 
--- src/CNNClassifier/config/configuration.py
+++ src/CNNClassifier/config/configuration.py
@@ -1,7 +1,8 @@
 from CNNClassifier.constants import *
 from CNNClassifier.utils.common import read_yaml, create_directories
 from CNNClassifier.entity.config_entity import DataIngestionConfig
-from CNNClassifier.entity.config_entity import (PrepareBaseModelConfig,PrepareCallbacksConfig)
+from CNNClassifier.entity.config_entity import (PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig)
+import os
 
 class ConfigManager:
     def __init__(
@@ -63,4 +64,27 @@ class ConfigManager:
                    checkpoint_model_filepath= Path(config.checkpoint_model_filepath)
              )
              return prepare_callback_config
+
+    def get_training_config(self)  -> TrainingConfig:
+               training = self.config.training
+               prepare_base_model = self.config.prepare_base_model
+               params = self.params
+               training_data = os.path.join(self.config.data_ingestion.unzip_dir,"Chicken-fecal-images")
+               create_directories([
+                      Path(training.root_dir)
+               ])
+    
+               training_config = TrainingConfig(
+                      root_dir=Path(training.root_dir),
+                      trained_model_path = Path(training.trained_model_path),
+                      updated_base_model_path = Path(prepare_base_model.updated_base_model_path),
+                      training_data = Path(training_data),
+                      params_epochs = params.EPOCHS,
+                      params_batch_size = params.BATCH_SIZE,
+                      params_is_augmentation= params.AUGUMENTATION,
+                      params_image_size= params.IMAGE_SIZE,
+                      params_learning_rate = params.LEARNING_RATE
+    
+               )
+               return training_config
            
